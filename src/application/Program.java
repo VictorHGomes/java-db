d13239501afc6add2de8c86fe855bbb6576d08b5
Victor Hugo <victor.couto@ufrpe.br>
2025-09-24 12:07:59 -0300

    criando classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+
     }
 }
